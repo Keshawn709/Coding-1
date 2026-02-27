a2de2ea0463d842e402cc54814d6dccdedffab4f
KeshawnW709 <kwhite535@boyslatin.net>
2026-02-27 13:44:02 +0000

    I did the review!

---
+++ Unit 1/BuiltInFunctionReview.py
@@ -0,0 +1,15 @@
+input("16"(str(16)))
+print(16)
+
+print("score"(str(85)))
+
+print("flout"(19.99))
+
+num1 = input("4")
+num2 = input("5")
+print("20")
+
+cost = 120.00
+print('It will cost' +cost(120.00)+ 'dollars to get everyone lunch')
+
+print("100 + 4")
