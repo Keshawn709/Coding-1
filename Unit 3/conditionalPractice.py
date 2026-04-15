891554fe3553a14a5cde8767debc5e2f5d1c58ea
KeshawnW709 <kwhite535@boyslatin.net>
2026-04-15 12:48:44 +0000

    new practice

--- Unit 3/conditionalPractice.py
+++ Unit 3/conditionalPractice.py
@@ -10,3 +10,14 @@ def coinMachine():
         print("dispencing 80 quarters")
     else:
         print("We're sorry to inform you, but this bill amount will not be accepted.")
+    
+
+def collegeAcceptance(gpa):
+    if gpa >= 3.5:
+        print("We're happy to inform you that you have been accepted to Keshawn University!")
+    elif gpa >= 2.8:
+        print("We're happy to inform you that you have been given a conditional offer to Keshawn University!")
+    else:
+        print("We regret to inform you that you have not been accepted to Keshawn University.")
+
+collegeAcceptance(3.6)
