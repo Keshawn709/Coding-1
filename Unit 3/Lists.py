83b9eccbd11a3309ada5c5f6658eb305942a0413
KeshawnW709 <kwhite535@boyslatin.net>
2026-04-16 12:46:00 +0000

    learned lists

---
+++ Unit 3/Lists.py
@@ -0,0 +1,23 @@
+# Lists can store multiple pieces of data such as booleans, string, integers,floats, and other lists.
+
+#It's sort of like a variable, but you can put more
+#data in it.
+
+name = "Keshawn"
+score = 100
+
+#syntax
+# 1. give the list a name, similar to how
+# we create names for variables
+# 2. Add 2 square brackets[]
+# 3. Put data types inside brackets, seperated by commas
+coding1Class = ['billy','stu','sidney']
+# lists can have ANY data type inside it
+#including other lists
+randomList = [10,
+              100.23,
+              True,
+              False,
+              'Do Something...',
+              coding1Class
+              ]
