7d6d82e4d3f567d11dae401475ae722f75862be4
KeshawnW709 <kwhite535@boyslatin.net>
2026-04-29 12:48:08 +0000

    Here is my listactivity

--- Unit 4/lists.py
+++ Unit 4/lists.py
@@ -17,5 +17,16 @@ print(blCoding)
 #lists are organized sequentially by index position
 #lists always start at zero
 
-print(blCoding[0])
-print(blCoding.count())
+print(blCoding[2])
+
+#list methods are functions that work on lists
+#remember: functions just mean code instructions.
+
+#the append method allows us to add an item at the END of a list
+blCoding.append("cyber security")
+print(blCoding)
+
+#the insert method allows us to add an item ANYWHERE in a list so long as we tell in which index to pass
+#it in
+blCoding.insert(2,10)
+print(blCoding)
