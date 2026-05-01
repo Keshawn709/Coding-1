5fd1d63dc53914f2dae728fc490cddd4f84b32de
KeshawnW709 <kwhite535@boyslatin.net>
2026-05-01 12:48:21 +0000

    while loops

---
+++ Unit 4/whileloopActivity.py
@@ -0,0 +1,19 @@
+# savings = 0
+# goal = 1000
+# while savings < goal:
+#     print("you currently have:" + str(savings))
+#     deposit = int(input("how much do you want to add?"))
+#     savings += deposit
+#     print("you have  " + str(savings) + "in your account")
+#     if savings >= goal:
+#         print("Congrats! You have enough for your trip!")
+#     else:
+#         print("Keep saving.")
+
+
+# My Own Work
+myPassword = "keshawn132"
+
+while myPassword != "keshawn132":
+    print("try again")
+    myPassword = input("what is pw")
